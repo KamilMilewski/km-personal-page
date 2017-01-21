1265b4e785c87afcf92bfd9ff633f00baed7516d
Kamil Milewski <kamil.milewski@hotmail.com>
2017-01-21 01:53:32 +0100

    Create first draft for dali and km-forum project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,14 @@
 # Static pages.
 class ProjectsController < ApplicationController
   def km_forum; end
+
   def machplot; end
+
   def dali; end
+
   def weather_station; end
+
   def reports; end
+
   def reflex; end
 end
